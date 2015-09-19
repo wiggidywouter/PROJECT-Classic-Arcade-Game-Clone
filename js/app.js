6b9654bc38c66876d83d71e6858681ea82f12c5a
Your Name <you@example.com>
2015-09-19 23:10:09 +0200

    allEnemies [] and player {}

--- js/app.js
+++ js/app.js
@@ -24,13 +24,20 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = {
+    update: function(){
+        //console.log('Player update call');
+    },
+    render: function(){
+        //console.log('Player render call');
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies =[];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
