a25fdd972814e5012d7ce33070c3dab8e4c114ec
Your Name <you@example.com>
2015-09-20 00:17:50 +0200

    player

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = 0;
+    this.y = 0;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -24,23 +26,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var player = {
-    update: function (){
-        //console.log('Player update call');
-    },
-    render: function (){
-        //console.log('Player render call');
-    },
-    handleInput: function () {
-        
+var Player = function(){
+    this.x = 202;
+    this.y = 400;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function (){
+    //console.log('Player update call');
+};
+
+Player.prototype.render = function (){
+    //console.log('Player render call');
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (keyCode) {
+    console.log(keyCode);
+    switch (keyCode) {
+        case 'up':
+            this.y -= 83;
+            break;
+        case 'down':
+            this.y += 83;
+            break;
+        case 'left':
+            this.x -= 101;
+            break;
+        case 'right':
+            this.x += 101;
+            break;
+
     }
 };
+
+var player = new Player();
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies =[];
+var allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -52,5 +78,8 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    if (allowedKeys[e.keyCode]) {
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
+
 });
